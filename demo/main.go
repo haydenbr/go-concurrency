9130c54fb2ec2b3c8c142b99776d094dd5a90662
Hayden Braxton <hayden.braxton@everactive.com>
2023-04-04 21:20:06 -0400

    init

---
+++ demo/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	receiveOrdersChan := make(chan order)
+	validOrderChan := make(chan order)
+	invalidOrderChan := make(chan invalidOrder)
+	go receiveOrders(receiveOrdersChan)
+	go validateOrders(receiveOrdersChan, validOrderChan, invalidOrderChan)
+
+	wg.Add(1)
+	go func() {
+		order := <-validOrderChan
+		fmt.Printf("valid order received: %v\n", order)
+		wg.Done()
+	}()
+	go func() {
+		order := <-invalidOrderChan
+		fmt.Printf("inalid order received: %v, error: %v\n", order.order, order.err)
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func validateOrders(inChan chan order, outChan chan order, errChan chan invalidOrder) {
+	order := <-inChan
+	if order.Quantity <= 0 {
+		errChan <- invalidOrder{order: order, err: errors.New("quantity must be greater than zero")}
+	} else {
+		outChan <- order
+	}
+}
+
+func receiveOrders(out chan order) {
+	for _, rawOrder := range rawOrders {
+		var newOrder order
+		err := json.Unmarshal([]byte(rawOrder), &newOrder)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		out <- newOrder
+	}
+}
+
+var rawOrders = []string{
+	`{"productCode": 1111, "quantity": 5, "status": 1}`,
+	`{"productCode": 2222, "quantity": 42.3, "status": 1}`,
+	`{"productCode": 3333, "quantity": 19, "status": 1}`,
+	`{"productCode": 4444, "quantity": 8, "status": 1}`,
+}
---
+++ demo/order.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type order struct {
+	ProductCode int
+	Quantity    float64
+	Status      orderStatus
+}
+
+func (o order) String() string {
+	return fmt.Sprintf(
+		"ProductCode: %v, Quantity: %v, Status: %v\n",
+		o.ProductCode, o.Quantity, orderStatusToString(o.Status),
+	)
+}
+
+func orderStatusToString(o orderStatus) string {
+	switch o {
+	case none:
+		return "none"
+	case new:
+		return "new"
+	case received:
+		return "received"
+	case reserved:
+		return "reserved"
+	case filled:
+		return "filled"
+	default:
+		return "unknown status"
+	}
+}
+
+type orderStatus int
+
+const (
+	none orderStatus = iota
+	new
+	received
+	reserved
+	filled
+)
+
+type invalidOrder struct {
+	order order
+	err   error
+}
