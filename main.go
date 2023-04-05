2fb7e6d11e06dfdbb037bba57c0f776fc48f7898
Hayden Braxton <hayden.braxton@everactive.com>
2023-04-04 22:16:46 -0400

    make channels directional

--- main.go
+++ main.go
@@ -17,20 +17,20 @@ func main() {
 	go validateOrders(receiveOrdersChan, validOrderChan, invalidOrderChan)
 
 	wg.Add(1)
-	go func() {
+	go func(validOrderChan <-chan order) {
 		order := <-validOrderChan
 		fmt.Printf("valid order received: %v\n", order)
 		wg.Done()
-	}()
-	go func() {
+	}(validOrderChan)
+	go func(invalidOrderChan <-chan invalidOrder) {
 		order := <-invalidOrderChan
 		fmt.Printf("inalid order received: %v, error: %v\n", order.order, order.err)
 		wg.Done()
-	}()
+	}(invalidOrderChan)
 	wg.Wait()
 }
 
-func validateOrders(inChan chan order, outChan chan order, errChan chan invalidOrder) {
+func validateOrders(inChan <-chan order, outChan chan<- order, errChan chan<- invalidOrder) {
 	order := <-inChan
 	if order.Quantity <= 0 {
 		errChan <- invalidOrder{order: order, err: errors.New("quantity must be greater than zero")}
@@ -39,7 +39,7 @@ func validateOrders(inChan chan order, outChan chan order, errChan chan invalidO
 	}
 }
 
-func receiveOrders(out chan order) {
+func receiveOrders(out chan<- order) {
 	for _, rawOrder := range rawOrders {
 		var newOrder order
 		err := json.Unmarshal([]byte(rawOrder), &newOrder)
